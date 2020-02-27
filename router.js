3fea0ceea543abed2daa443fb83c5c66ff439e7d
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-02-27 12:53:24 -0300

    Agregado handlers

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
-function route(path) {
+function route(path, handle) {
 	console.log('routeando request a '+path);
+	if (typeof handle[path] === 'function') {
+		return handle[path]();
+	} else {
+		console.log('No hay handler definido para' + path);
+	}
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var url = require('url');
 
-function iniciar(route) {
+function iniciar(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log(`Request a ${pathname} recibido.`);
 
-		route(pathname);
+		route(pathname, handle);
 
 		response.writeHead(200, {'Content-Type': 'text/html'});
 		response.write('holis');

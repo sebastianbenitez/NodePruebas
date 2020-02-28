ca6fe497d46fdd46cf07c7000aa9c09ef4b1b8fa
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-02-28 09:26:22 -0300

    Actualizados handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function iniciar() {
 	console.log('Invocado el handler de Iniciar.');
+	return 'Iniciar';
 }
 
 function subir() {
 	console.log('Invocado el handler de Subir.');
+	return 'Subir';
 }
 
 exports.iniciar = iniciar;
